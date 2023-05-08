f4856f2c5678c9edac816025c7d9f3d43e5e4134
Hasham <hashamullah4@gmail.com>
2023-05-08 13:50:11 +0500

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import * as React from 'react'
 // 1. import `ChakraProvider` component
-import { ChakraProvider, Text } from '@chakra-ui/react'
+import { ChakraProvider, Text, Progress } from '@chakra-ui/react'
 
 function App() {
   // 2. Wrap ChakraProvider at the root of your app
   return (
     <ChakraProvider>
+      <Progress size='lg' isIndeterminate />
       <Text fontSize='6xl' textAlign={"center"} >Landing Page</Text>
+      <Progress size='lg' isIndeterminate />
     </ChakraProvider>
   )
 }
